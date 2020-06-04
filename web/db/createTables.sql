c1d02dadbfd8d244d79b9e629eeb655da26dba7e
aaron9902 <49785208+aaron9902@users.noreply.github.com>
2020-06-04 23:29:44 +1000

    CREATE TABLE edited

--- web/db/createTables.sql
+++ web/db/createTables.sql
@@ -1,4 +1,4 @@
-DROP TABLE USERS;
+/*DROP TABLE USERS;
 DROP TABLE LOGHISTORY;
 DROP TABLE PRODUCT;
 DROP TABLE CATEGORY;
@@ -7,117 +7,134 @@ DROP TABLE ORDERS;
 DROP TABLE ORDERLINE;
 DROP TABLE PAMENT;
 DROP TABLE PAYMENT;
-DROP TABLE SHIPMENT;
+DROP TABLE SHIPMENT; */
 
 
 CREATE TABLE USERS (
-"id"            INTEGER NOT NULL PRIMARY KEY
+id              INTEGER NOT NULL PRIMARY KEY
                     GENERATED ALWAYS AS IDENTITY
-                    (START WITH 1, INCREMENT BY 1) ON DELETE CASCADE, 
-"fName"         VARCHAR(10),
-"lName"         VARCHAR(10),
-"password"      VARCHAR(20),
-"email"         VARCHAR(50) NOT NULL,
-"mobileNum"     VARCHAR(20),
-"address"       VARCHAR(50),
-"usertype"      VARCHAR(10),
-"active"        BOOLEAN
+                    (START WITH 1, INCREMENT BY 1), 
+fName           VARCHAR(10),
+lName           VARCHAR(10),
+password        VARCHAR(20),
+email           VARCHAR(50) NOT NULL,
+mobileNum       VARCHAR(20),
+address         VARCHAR(50),
+usertype        VARCHAR(10),
+active          BOOLEAN
 );
 
 CREATE TABLE LOGHISTORY
 (
-"id"            INTEGER NOT NULL PRIMARY KEY,
-"SignInDate"    DATE,
-"startTime"     TIME,
-"endTime"       TIME,
-"duration"      TIME
-);
-
-CREATE TABLE PRODUCT
-(
-"productID"     INTEGER NOT NULL PRIMARY KEY
+id              INTEGER NOT NULL PRIMARY KEY 
                     GENERATED ALWAYS AS IDENTITY
                     (START WITH 1, INCREMENT BY 1), 
-"categoryID"    INTEGER REFERENCES CATEGORY (categoryID);
-"productName"   VARCHAR(20),
-"productPrice"  Double(7),
-"description"   VARCHAR(300),
-"status"        VARCHAR(1),
-"quantity"      INTEGER(5),
-/* FOREIGN KEY (supplierID), */
-FOREIGN KEY (categoryID) REFERENCES CATEGORY (categoryID)
+SignInDate      TIMESTAMP,
+startTime       TIMESTAMP,
+endTime         TIMESTAMP,
+duration        TIMESTAMP
 );
 
 CREATE TABLE CATEGORY
 (
-"categoryID"    INTEGER NOT NULL PRIMARY KEY,
-"categoryName"  VARCHAR(10),
-"description"   VARCHAR(100)
+categoryID      INTEGER NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY
+                                        (START WITH 1, INCREMENT BY 1), 
+categoryName    VARCHAR(10),
+description     VARCHAR(100)
 );
+
+CREATE TABLE PRODUCT
+(
+productID       INTEGER NOT NULL PRIMARY KEY
+                    GENERATED ALWAYS AS IDENTITY
+                    (START WITH 1, INCREMENT BY 1), 
+categoryID      INTEGER,
+productName     VARCHAR(20),
+productPrice    DOUBLE,
+description     VARCHAR(300),
+status          VARCHAR(1),
+quantity        INTEGER
+);
+
+ALTER TABLE PRODUCT ADD FOREIGN KEY (categoryID) REFERENCES CATEGORY (categoryID);
+
 
 CREATE TABLE BASKET 
 (
-"basketID"      INTEGER NOT NULL PRIMARY KEY
+basketID        INTEGER NOT NULL PRIMARY KEY
                     GENERATED ALWAYS AS IDENTITY
                         (START WITH 1, INCREMENT BY 1),
-"totalProductQty" INTEGER(100),
-"totalAmount"     INTEGER(8),
-"selectedProducts"INTEGER(100),
-FOREIGN KEY (customerID) REFERENCES USERS (id),
-FOREIGN KEY (productID) REFERENCES PRODUCT (productID)
+customerID      INTEGER,
+productID       INTEGER,
+totalProductQty INTEGER,
+totalAmount     INTEGER,
+selectedProducts INTEGER
 );
+
+ALTER TABLE BASKET ADD FOREIGN KEY (customerID) REFERENCES USERS (id);
+ALTER TABLE BASKET ADD FOREIGN KEY (productID) REFERENCES PRODUCT (productID);
 
 CREATE TABLE ORDERS
 (
-"orderID"       INTEGER NOT NULL PRIMARY KEY
+orderID         INTEGER NOT NULL PRIMARY KEY
                     GENERATED ALWAYS AS IDENTITY
                         (START WITH 1, INCREMENT BY 1),
-"orderDate"     DATE(8),
-"orderStatus"   VARCHAR(10),
-FOREIGN KEY (customerID) REFERENCES USERS (id)
+customerID      INTEGER,
+orderDate       TIMESTAMP,
+orderStatus     VARCHAR(10)
 );
 
-CREATE TABLE ORDERLINE
-(
-"orderID"       INTEGER REFERENCES ORDERS (orderID),
-"productID"     INTEGER REFERENCES PRODUCT (productID),
-"paymentID"     INTEGER REFERENCES PAYMENT (paymentID),
-"shipmentID"    INTEGER REFERENCES SHIPMENT (shipmentID),    
-"productQty"    INTEGER(100),
-"total"         INTEGER(100),
-"shipmentDate"  TIMESTAMP,
-PRIMARY KEY (orderID, productID, paymentID, shipmentID)
-);
+ALTER TABLE ORDERS ADD FOREIGN KEY (customerID) REFERENCES USERS (id);
+
 
 CREATE TABLE PAYMENT
 (
-"paymentID"     INTEGER NOT NULL PRIMARY KEY
+paymentID       INTEGER NOT NULL PRIMARY KEY
                     GENERATED ALWAYS AS IDENTITY
                         (START WITH 1, INCREMENT BY 1),
-"paymentTime"   TIMESTAMP,
-"amount"        DOUBLE,
-"description"   VARCHAR(30),
-"paymentType"   VARCHAR(10),
-"paymentStatus" VARCHAR(10),
-);
-
-CREATE TABLE REVIEW
-(
-"reviewID"      INTEGER NOT NULL PRIMARY KEY
-                    GENERATED ALWAYS AS IDENTITY
-                        (START WITH 1, INCREMENT BY 1),
-"productRating" INTEGER(5),
-"reviewText"    VARCHAR(300),
-FOREIGN KEY (customerID) REFERENCES USERS (id),
-FOREIGN KEY (productID) REFERENCES PRODUCT (productID)
+paymentTime     TIMESTAMP,
+amount          DOUBLE,
+description     VARCHAR(30),
+paymentType     VARCHAR(10),
+paymentStatus   VARCHAR(10)
 );
 
 CREATE TABLE SHIPMENT
 (
-"shipmentID"      INTEGER NOT NULL PRIMARY KEY
+shipmentID      INTEGER NOT NULL PRIMARY KEY
                     GENERATED ALWAYS AS IDENTITY
                         (START WITH 1, INCREMENT BY 1),
-"courier"         VARCHAR(30),
-"trackingCode"    VARCHAR(10),
-"shipmentAddress" VARCHAR(100)
+courier         VARCHAR(30),
+trackingCode    VARCHAR(10),
+shipmentAddress VARCHAR(100)
 );
+
+CREATE TABLE ORDERLINE
+(
+orderID         INTEGER REFERENCES ORDERS (orderID),
+productID       INTEGER REFERENCES PRODUCT (productID),
+paymentID       INTEGER REFERENCES PAYMENT (paymentID),
+shipmentID      INTEGER REFERENCES SHIPMENT (shipmentID),    
+productQty      INTEGER,
+total           INTEGER,
+shipmentDate    TIMESTAMP,
+PRIMARY KEY (orderID, productID, paymentID, shipmentID)
+);
+
+
+
+CREATE TABLE REVIEW
+(
+reviewID      INTEGER NOT NULL PRIMARY KEY
+                    GENERATED ALWAYS AS IDENTITY
+                        (START WITH 1, INCREMENT BY 1),
+customerID      INTEGER,
+productID       INTEGER,
+productRating INTEGER,
+reviewText    VARCHAR(300)
+);
+
+ALTER TABLE REVIEW ADD FOREIGN KEY (customerID) REFERENCES USERS (id);
+ALTER TABLE REVIEW ADD FOREIGN KEY (productID) REFERENCES PRODUCT (productID);
+
+
