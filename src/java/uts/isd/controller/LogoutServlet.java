8a965bdc4f5935f236519d84c9c07b75ed5bf161
Heeyoon Koo <Heeyoon.koo-1@student.uts.edu.au>
2020-06-06 17:58:19 +0900

    Update LogoutServlet.java
    sorry forgot to set user's active as false
    just added

--- src/java/uts/isd/controller/LogoutServlet.java
+++ src/java/uts/isd/controller/LogoutServlet.java
@@ -8,6 +8,7 @@ import java.util.logging.Logger;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import uts.isd.dao.DBManager;
+import uts.isd.model.User;
  
 
 public class LogoutServlet extends HttpServlet {
@@ -23,9 +24,11 @@ public class LogoutServlet extends HttpServlet {
         HttpSession session = request.getSession();
         DBManager manager = (DBManager) session.getAttribute("manager");
         int userId =Integer.parseInt(request.getParameter("userId"));
+        User user = (User) session.getAttribute("user");
         
         try {
             manager.addUserLog(userId,"Logout");
+            user.setActive(false);
            session = request.getSession(false);
             if (session != null) {
             session.removeAttribute("user");
