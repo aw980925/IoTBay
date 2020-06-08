9626f6d830869272d91bf5d76b6a8f0fbd209a7c
neeseong <54238922+neeseong@users.noreply.github.com>
2020-06-08 19:47:50 +1000

    In progress, Add "AddProductServlet"

---
+++ src/java/uts/isd/controller/AddProductServlet.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.isd.controller;
+
+import java.io.IOException;
+
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import uts.isd.model.User;
+import uts.isd.dao.DBManager;
+/**
+ *
+ * @author heeseong
+ */
+public class AddProductServlet extends HttpServlet{
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    
+    
+    }
+}
