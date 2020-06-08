516e242d4e538b39cb80e64f3070d422d716da79
aw980925 <62045307+aw980925@users.noreply.github.com>
2020-06-08 16:14:15 +1000

    fixed addusersuccess

---
+++ src/java/uts/isd/controller/SearchUserServlet.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+   package uts.isd.controller;
+
+   import java.io.IOException;
+   import java.sql.Connection;
+   import java.sql.SQLException;
+   import java.util.logging.Level;
+   import java.util.logging.Logger;
+   import javax.servlet.ServletException;
+   import javax.servlet.http.HttpServlet;
+   import javax.servlet.http.HttpServletRequest;
+   import javax.servlet.http.HttpServletResponse;
+   import javax.servlet.http.HttpSession;
+   import uts.isd.dao.*;
+   import uts.isd.model.*;
+
+public class SearchUserServlet extends HttpServlet{
+    
+    
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    {
+        
+        //1- retrieve the current session
+        HttpSession session = request.getSession();
+        //2- create an instance of the Validator class    
+        Validator validator = new Validator();
+        //3- capture the posted email      
+        String name = request.getParameter("fName");
+        String number = request.getParameter("mobileNum");
+        //4- capture the posted password    
+
+        
+        User user = null; 
+        validator.clear(session);
+        
+        //5- retrieve the manager instance from session      
+        DBManager manager = (DBManager) session.getAttribute("manager");
+        
+        
+
+        //6- find product by name and type
+        
+        if(user != null){
+            
+            session.setAttribute("user", user);
+            request.getRequestDispatcher("/admin/index,jsp").include(request,response);
+            
+        } else {
+            session.setAttribute("exitUserErr", "User doesn't exist in the database!");
+            request.getRequestDispatcher("/admin/index.jsp").include(request, response);
+            
+        }
+    }
+}
